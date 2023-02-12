37695b4d83c9630a45d3524e0e8f87fe294315b2
ivanrodriguez <ivanoviicrm2@gmail.com>
2023-02-13 00:08:36 +0100

    starting repo

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
 import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get(`/`, async (req, res) => {
+  res.json({ message: 'Ok!' });
+});
+
+const server = app.listen(3000, () =>
+  console.log(`🚀 Server is running at: http://localhost:${port}`)
+);
